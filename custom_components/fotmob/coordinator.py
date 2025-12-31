cddcf5f779dba9fe2174abb644f96b5be12100e6
kidabcd <airafleugim@gmail.com>
2025-12-31 16:10:45 +0000

    updates

--- custom_components/fotmob/coordinator.py
+++ custom_components/fotmob/coordinator.py
@@ -1,4 +1,6 @@
-from datetime import timedelta
+
+
+from datetime import timedelta, date
 import aiohttp
 import logging
 
@@ -6,10 +8,10 @@ from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
 _LOGGER = logging.getLogger(__name__)
 
-API_URL = "https://api-football-v1.p.rapidapi.com/v3/fixtures?live=all"
+API_URL = f"https://v3.football.api-sports.io/fixtures?date={date.today()}"
 API_HEADERS = {
     "X-RapidAPI-Key": "94ef4ba0647ea3e47b957dbc4e9ac5d5",
-    "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com"
+    "X-RapidAPI-Host": "v3.football.api-sports.io"
 }
 
 class FotMobCoordinator(DataUpdateCoordinator):
@@ -18,17 +20,19 @@ class FotMobCoordinator(DataUpdateCoordinator):
             hass,
             logger=_LOGGER,
             name="FotMob",
-            update_interval=timedelta(seconds=60),  # atualiza a cada minuto
+            update_interval=timedelta(minutes=15)
         )
 
     async def _async_update_data(self):
-        """Busca partidas ao vivo da API-Football."""
+        """Busca partidas do dia da API-Football v3."""
         try:
             async with aiohttp.ClientSession(headers={"User-Agent": "HomeAssistant/1.0"}) as session:
                 async with session.get(API_URL, headers=API_HEADERS) as resp:
                     resp.raise_for_status()
                     data = await resp.json()
+                    # Retorna apenas lista de partidas
                     return data.get("response", [])
         except Exception as e:
             _LOGGER.error("Erro ao buscar partidas: %s", e)
             raise
+
