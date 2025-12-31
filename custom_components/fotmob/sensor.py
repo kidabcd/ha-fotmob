92aa1f259307f1966b4e665983d65915a051dba6
kidabcd <airafleugim@gmail.com>
2025-12-31 15:32:16 +0000

    update sensors

--- custom_components/fotmob/sensor.py
+++ custom_components/fotmob/sensor.py
@@ -1,9 +1,20 @@
 from homeassistant.components.sensor import SensorEntity
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-class FotMobLiveMatchesSensor(SensorEntity):
+async def async_setup_entry(hass, entry, async_add_entities):
+    coordinator = hass.data["fotmob"][entry.entry_id]
+    async_add_entities([FotMobLiveMatchesSensor(coordinator)])
+
+class FotMobLiveMatchesSensor(CoordinatorEntity, SensorEntity):
     _attr_has_entity_name = True
 
     def __init__(self, coordinator):
-        self.coordinator = coordinator
+        super().__init__(coordinator)
         self._attr_unique_id = "fotmob_live_matches"
         self._attr_name = "Jogos ao Vivo"
+
+    @property
+    def native_value(self):
+        if not self.coordinator.data:
+            return 0
+        return len(self.coordinator.data.get("liveMatches", []))
