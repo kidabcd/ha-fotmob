1f6cf04629c86cd023bf116c5b540898f921efd4
kidabcd <airafleugim@gmail.com>
2025-12-31 15:27:36 +0000

    update sensor

--- custom_components/fotmob/sensor.py
+++ custom_components/fotmob/sensor.py
@@ -1,10 +1,9 @@
 from homeassistant.components.sensor import SensorEntity
 
 class FotMobLiveMatchesSensor(SensorEntity):
+    _attr_has_entity_name = True
+
     def __init__(self, coordinator):
         self.coordinator = coordinator
+        self._attr_unique_id = "fotmob_live_matches"
         self._attr_name = "Jogos ao Vivo"
-
-    @property
-    def native_value(self):
-        return len(self.coordinator.data.get("liveMatches", []))
